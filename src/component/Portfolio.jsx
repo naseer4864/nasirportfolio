5799a536d45a6b7c42fa9bb3c6536279942e1dce
naseer4864 <naseernoor4864@gmail.com>
2023-03-13 17:43:28 +0100

    first commit

--- src/component/Portfolio.jsx
+++ src/component/Portfolio.jsx
@@ -7,28 +7,28 @@ const Portfolio = () => {
             </div>
             <div className="recent-works-container">
                 <div className="recent-1">
-                    <img src="https://i.ibb.co/PjxnSVy/Screen-Shot-2023-03-13-at-11-05-35-AM.png" alt="" />
-                    <a href="https://as-salam-bedding.vercel.app/">DEMO</a>
+                    <img src="https://i.ibb.co/PjxnSVy/Screen-Shot-2023-03-13-at-11-05-35-AM.png" alt=""  />
+                    <a href="https://as-salam-bedding.vercel.app/" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
                 <div className="recent-2">
                     <img src="https://i.ibb.co/9w8F2M4/Screen-Shot-2023-03-13-at-11-07-01-AM.png" alt="" />
-                    <a href="https://weathercheck-eosin.vercel.app/">DEMO</a>
+                    <a href="https://weathercheck-eosin.vercel.app/" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
                 <div className="recent-3">
                     <img src="https://i.ibb.co/nnQ76T9/Screen-Shot-2023-03-13-at-11-07-37-AM.png" alt="" />
-                    <a href="https://taskly-omega.vercel.app/">DEMO</a>
+                    <a href="https://taskly-omega.vercel.app/" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
                 <div className="recent-4">
                     <img src="https://i.ibb.co/2M4K5RS/Screen-Shot-2023-03-13-at-11-08-18-AM.png" alt="" />
-                    <a href="http://my-contacts-list.vercel.app/">DEMO</a>
+                    <a href="http://my-contacts-list.vercel.app/" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
                 <div className="recent-5">
                     <img src="https://i.ibb.co/cc3vdqY/Screen-Shot-2023-03-13-at-11-09-21-AM.png" alt="" />
-                    <a href="https://naseer4864.github.io/jimonemotors/">DEMO</a>
+                    <a href="https://naseer4864.github.io/jimonemotors/" target="_blank"  rel="noopener noreferrer">DEMO</a>
                 </div>
                 <div className="recent-6">
                     <img src="https://i.ibb.co/kDGJJjn/Screen-Shot-2023-03-13-at-11-10-38-AM.png" alt="" />
-                    <a href="http://codewithnaseer.vercel.app/">DEMO</a>
+                    <a href="http://codewithnaseer.vercel.app/" target="_blank" rel="noopener noreferrer">DEMO</a>
                 </div>
             </div>
         </div>
