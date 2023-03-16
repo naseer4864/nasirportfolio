5c39362fb9dc9b971a87b6eb9864dd52335970f7
naseer4864 <naseernoor4864@gmail.com>
2023-03-16 17:23:20 +0100

    first commit

--- src/component/footer.jsx
+++ src/component/footer.jsx
@@ -1,10 +1,49 @@
+import { useNavigate } from "react-router-dom";
+
+
 const Footer = () => {
+  const navigate= useNavigate();
+  const handleNav = () => {
+    navigate("/contact")
+  }
   return (
     <div className="footer-container">
-      <p>
-        Copyright &copy;2023 All Rights Reserved | This is made with &hearts; by
-        NaseerCode
-      </p>
+    <h2>Intrested in working with me ?</h2>
+    <span>Do you have a project in mind and would love to bring it to live? Then let's talk.</span>
+    <button onClick={handleNav}>Let's talk</button>
+      <div className="footer-app">
+            <a href=" https://wa.me/2347036676508">
+              <img
+                src="https://i.ibb.co/YfyN3V8/whatsapp.png"
+                alt=""
+                className="whats"
+                />
+            </a>
+            <a href="https://www.facebook.com/nassopeyemi">
+              <img
+                src="https://i.ibb.co/M8Hyg7n/facebook.png"
+                alt=""
+                className="face"
+                />
+            </a>
+            <a href="https://www.instagram.com/naseeradeniyi/">
+              <img
+                src="https://i.ibb.co/rsFxRLw/instagram.png"
+                alt=""
+                className="insta"
+                />
+            </a>
+            <a href="https://twitter.com/naseeroladayo/">
+              <img
+                src="https://i.ibb.co/5Yv5KpP/twitter.png"
+                alt=""
+                className="twitt"
+                />
+            </a>
+          </div>
+                <p>
+                  &copy;2023 All Rights Reserved | with &hearts; NaseerCode
+                </p>
     </div>
   );
 };
